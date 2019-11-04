84952eda7fa702206c82924eae597bc0ec636134
Jelmer van Arnhem <Jelmerro@users.noreply.github.com>
2019-11-04 22:33:11 +0100

    improve build command

--- build.js
+++ build.js
@@ -2,12 +2,19 @@
 "use strict"
 
 const builder = require("electron-builder")
-
-builder.build({
-    "win": [],
-    "mac": [],
-    "linux": []
-}).then(e => {
+const builds = {}
+process.argv.slice(1).forEach(a => {
+    if (a === "--linux") {
+        builds.linux = []
+    }
+    if (a === "--win") {
+        builds.win = []
+    }
+    if (a === "--mac") {
+        builds.mac = []
+    }
+})
+builder.build(builds).then(e => {
     console.log(e)
 }).catch(e => {
     console.error(e)
