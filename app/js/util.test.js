a5c38c307d1cfdc48b3a279aa4348292dfeab617
Jelmerro <Jelmerro@users.noreply.github.com>
2020-03-16 00:14:15 +0100

    rework util test

--- app/js/util.test.js
+++ app/js/util.test.js
@@ -1,28 +1,128 @@
+/*
+* Vieb - Vim Inspired Electron Browser
+* Copyright (C) 2020 Jelmer van Arnhem
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 /* global test expect */
 "use strict"
 
-const util = require("./util")
-const isUrl = util.isUrl
-const urls = [["google.com", true, "No www."],
-    ["h%^*&TD{yfsd^.com", false, "Invalid characters"],
-    ["localhost", true, "Localhost"],
-    ["localhost:555", true, "Localhost with port"],
-    ["127.0.0.1", true, "Loopback"],
-    ["hoi:test@hoi.com", false, "A username and password in url"],
-    ["www.youtube.com/test?id=1&uid=1", true, "2 get parameters"],
-    ["192.168.2.5:25000", true, "Correct ip and port"],
-    ["192.168.2.5:0002", false, "Too low port"],
-    ["192.168.2.5:000", false, "Zero low port"],
-    ["192.168.2.5:70002", false, "Too high port"],
-    ["192.168.2.5:3", false, "Too short port"],
-    ["192.168.2.5:333333", false, "Too long port"],
-    ["raspberrypi/test", false, "A hostname and page"],
-    ["hoi:test@www.*.com", false, "Invalid with login"],
-    ["roger:@lol.com", false, "Invalid due to no password"],
-    ["roger@lol.com", true, "Username only but valid"],
-    ["hoi@test@hoi.com", false, "Double @ sign is invalid"]]
-for (let i = 0; i < urls.length; i++) {
-    test(`test url: ${urls[i][0]}, testing: ${urls[i][2]}`, () => {
-        expect(isUrl(urls[i][0])).toBe(urls[i][1])
+const UTIL = require("./util")
+const urlTests = [
+    {
+        "url": "google.com",
+        "valid": true,
+        "reason": "Valid url without www."
+    },
+    {
+        "url": "h%^*&TD{yfsd^.com",
+        "valid": false,
+        "reason": "Invalid url because of invalid characters"
+    },
+    {
+        "url": "localhost",
+        "valid": true,
+        "reason": "Localhost should always be valid"
+    },
+    {
+        "url": "localhost:555",
+        "valid": true,
+        "reason": "Localhost with port should also be valid"
+    },
+    {
+        "url": "127.0.0.1",
+        "valid": true,
+        "reason": "Ip addresses should be valid"
+    },
+    {
+        "url": "34.250.34.34",
+        "valid": true,
+        "reason": "Ip addresses should be valid"
+    },
+    {
+        "url": "234.435.0.1",
+        "valid": false,
+        "reason": "Invalid ip addresses should be detected as such"
+    },
+    {
+        "url": "www.youtube.com/test?id=1&uid=1",
+        "valid": true,
+        "reason": "Query params are allowed"
+    },
+    {
+        "url": "192.168.2.5:25000",
+        "valid": true,
+        "reason": "Correct ip and port"
+    },
+    {
+        "url": "192.168.2.5:0002",
+        "valid": false,
+        "reason": "Port below the valid range of 11 to 65535"
+    },
+    {
+        "url": "192.168.2.5:000",
+        "valid": false,
+        "reason": "Port below the valid range of 11 to 65535"
+    },
+    {
+        "url": "192.168.2.5:66536",
+        "valid": false,
+        "reason": "Port above the valid range of 11 to 65535"
+    },
+    {
+        "url": "192.168.2.5:000333",
+        "valid": false,
+        "reason": "Port number has too many characters"
+    },
+    {
+        "url": "raspberrypi/test",
+        "valid": false,
+        "reason": "A hostname without a toplevel domain is considered invalid"
+    },
+    {
+        "url": "hoi:test@hoi.com",
+        "valid": false,
+        "reason": "A password in url is not allowed"
+    },
+    {
+        "url": "user:@lol.com",
+        "valid": false,
+        "reason": "Invalid due the inclusion of ':' which indicates a password"
+    },
+    {
+        "url": "user@lol.com",
+        "valid": true,
+        "reason": "Providing only the username is allowed in urls"
+    },
+    {
+        "url": "hello@test@hoi.com",
+        "valid": false,
+        "reason": "Double @ sign is invalid"
+    },
+    {
+        "url": "hello--dashes.com",
+        "valid": true,
+        "reason": "Double dashes are unusual but allowed"
+    },
+    {
+        "url": "hello---dashes.com",
+        "valid": false,
+        "reason": "Triple dashes are not allowed"
+    }
+]
+urlTests.forEach(urlTest => {
+    test(`Testing "${urlTest.url}": ${urlTest.reason}`, () => {
+        expect(UTIL.isUrl(urlTest.url)).toBe(urlTest.valid)
     })
-}
+})
