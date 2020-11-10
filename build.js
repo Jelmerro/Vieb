9d3778f27e6ed166af998cc15429820bb88730a4
Jelmerro <Jelmerro@users.noreply.github.com>
2020-11-11 00:26:06 +0100

    use archiver to zip mac releases instead of electron-builder to fix unzip issues

--- build.js
+++ build.js
@@ -3,7 +3,19 @@
 
 const builder = require("electron-builder")
 const rimraf = require("rimraf").sync
+const archiver = require("archiver")
+const fs = require("fs")
+const version = JSON.parse(fs.readFileSync("package.json").toString()).version
 const builds = {}
+
+const isDir = loc => {
+    try {
+        return fs.statSync(loc).isDirectory()
+    } catch (e) {
+        console.log(e)
+        return false
+    }
+}
 
 rimraf("dist/")
 process.argv.slice(1).forEach(a => {
@@ -18,6 +30,16 @@ process.argv.slice(1).forEach(a => {
     }
 })
 builder.build(builds).then(e => {
+    if (isDir("dist/mac/Vieb.app/")) {
+        rimraf("dist/Vieb-*-mac.zip")
+        const stream = fs.createWriteStream(`dist/Vieb-${version}-mac.zip`)
+        const archive = archiver("zip", {"zlib": {"level": 9}})
+        archive.pipe(stream)
+        archive.directory("dist/mac/Vieb.app/", "Vieb.app")
+        archive.file("README.md", {"name": "README.md"})
+        archive.file("LICENSE", {"name": "LICENSE"})
+        archive.finalize()
+    }
     console.log(e)
 }).catch(e => {
     console.error(e)
