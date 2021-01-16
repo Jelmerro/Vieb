326748ec84395bb5e8ecc99f0a7002b0f4d28f7f
Jelmer van Arnhem <Jelmerro@users.noreply.github.com>
2021-01-16 14:28:23 +0100

    only apply url checks to the right type and also check for empty href

--- app/js/preloads/follow.js
+++ app/js/preloads/follow.js
@@ -253,11 +253,14 @@ const parseElement = (element, type) => {
         return null
     }
     // The element should be clickable and is returned in a parsed format
-    if (element.href === window.location.href) {
-        type = "other"
-    }
-    if (element.href === `${window.location.href}#`) {
-        type = "other"
+    if (type === "url") {
+        if (!element.href) {
+            type = "other"
+        } else if (element.href === window.location.href) {
+            type = "other"
+        } else if (element.href === `${window.location.href}#`) {
+            type = "other"
+        }
     }
     return {
         "url": String(element.href || ""),
