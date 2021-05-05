c260c4620da88b5f39b2e042230d60b4a7d9edad
Jelmer van Arnhem <Jelmerro@users.noreply.github.com>
2021-05-05 17:40:53 +0200

    fix missing rename for rotate action

--- app/renderer/actions.js
+++ app/renderer/actions.js
@@ -347,7 +347,7 @@ const exploreHistoryNext = () => {
 
 const rotateSplitWindowForward = () => {
     const {rotateForward} = require("./pagelayout")
-    rotate()
+    rotateForward()
 }
 
 const rotateSplitWindowBackward = () => {
