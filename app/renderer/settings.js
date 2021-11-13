adbde02322d3fbd808c925f344596c4bec9cdeb9
Mauricio Pérez D <perezd.mauricio@gmail.com>
2021-11-13 16:47:57 -0600

    Add: followelement no longer accepts wrong values

--- app/renderer/settings.js
+++ app/renderer/settings.js
@@ -605,6 +605,17 @@ const checkOther = (setting, value) => {
     if (setting === "suggestorder") {
         return checkSuggestOrder(value)
     }
+    if (setting === "followelement") {
+        const ok = ["url", "onclick", "inputs-insert", "inputs-click", "other"]
+        for (const element of value.split(",")) {
+            if (!ok.includes(element)) {
+                notify(`Invalid element type passed:${element}, `
+                       + `must be any combination of: url, onclick,
+                          inputs-insert, inputs-click or other`)
+                return false
+            }
+        }
+    }
     return true
 }
 
