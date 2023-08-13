9ff35a2c4202619f2db933850d98d260c284d9bd
Jelmer van Arnhem <Jelmerro@users.noreply.github.com>
2023-08-14 01:18:31 +0200

    add table of contents to markdownviewer

--- app/preload/index.js
+++ app/preload/index.js
@@ -24,11 +24,16 @@ require("./follow")
 
 const {pathToSpecialPageName} = require("../util")
 const specialPage = pathToSpecialPageName(window.location.href)
-const skipProtocols = ["sourceviewer:", "readerview:", "markdownviewer:"]
+const viewerProtocols = ["sourceviewer:", "readerview:", "markdownviewer:"]
 if (specialPage?.name) {
     // Load the special page specific JavaScript
     require(`./${specialPage.name}`)
-} else if (!skipProtocols.some(p => window.location.href.startsWith(p))) {
+} else if (viewerProtocols.some(p => window.location.href.startsWith(p))) {
+    // Load the viewer specific functions
+    if (window.location.href.startsWith("markdownviewer:")) {
+        require("./markdownviewer")
+    }
+} else {
     // Load the failed page information handler for nonspecial pages
     require("./failedload")
     // Load the local directory browser for nonspecial pages
