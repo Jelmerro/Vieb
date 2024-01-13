c05a6ad3a45bd7548f936993a949386f3c09e823
Jelmer van Arnhem <Jelmerro@users.noreply.github.com>
2024-01-13 22:53:51 +0100

    lint update script

--- app/blocklists/update.sh
+++ app/blocklists/update.sh
@@ -1,4 +1,5 @@
-cd $(dirname $(realpath $0))
+#!/usr/bin/env bash
+cd "$(dirname "$(realpath "$0")")" || exit
 curl https://easylist-downloads.adblockplus.org/easylist.txt > easylist.txt
 curl https://easylist-downloads.adblockplus.org/easyprivacy.txt > easyprivacy.txt
 curl https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters.txt > ublockfilters.txt
